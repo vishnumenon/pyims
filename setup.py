b889854775dba9b41b92de7c83eaef55b0b376a5
Vishnu Menon <me@vishnumenon.com>
2018-05-20 22:00:08 -0400

    License and config for packaging

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name="pyims",
     version='0.1',
     description='A python wrapper for the IMS Word Sense Disambiguation tool (Zhong and Ng, 2010)',
     url='http://github.com/vishnumenon/pyims',
     author="Vishnu Menon",
     author_email="me@vishnumenon.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license='MIT',
-    packages=['pyims'],
+    packages=setuptools.find_packages(),
     install_requires=[
         'nltk',
     ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
     zip_safe=False)
